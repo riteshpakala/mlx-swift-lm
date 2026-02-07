6099474c18ad51907820737ad056b7c7f2569fbe
Rao <riteshpakala@users.noreply.github.com>
2026-02-06 17:30:25 -0800

    update to personal fork of transformers

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.30.3")),
         .package(
-            url: "https://github.com/huggingface/swift-transformers",
-            .upToNextMinor(from: "1.1.6")
+            url: "https://github.com/riteshpakala/swift-transformers",
+            branch: "main"
         ),
     ],
     targets: [
